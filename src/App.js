1a461e381b622e919973fb0f26dd8f6717e29de7
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2021-05-03 11:51:21 -0400

    Added the logo and the header for shop and contact section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.components'
+import Header from './components/header/header.component'
 
 const HatsPage = () => (
   <div>
@@ -12,8 +13,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
